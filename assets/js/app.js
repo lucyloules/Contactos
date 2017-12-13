3a1952ad9764af1de5d80c23d3acd244eec2a248
Lucyloules <lucia.lagos@gmail.com>
2017-12-13 11:38:19 -0300

    se incluye funciones jquery, falta corregir modal y jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,42 @@
   $(document).ready(function(){
     // Se copia de materialize el modal,funcion click  y se modifica para nuestro ejercicio
-    $('.modal').modal();
+    $('.modal').modal();//se llama y levanta al modal
 
+    $('#add').click(function() {//evento click en botón "+"
+   //creo variables
+    var name = $('.name').val(); //guardo el nombre del contacto
+    var phone = $('.phone').val(); //guardo el telefono del contaco
+    var email = $('.email').val();//guardo el email del contaco
+    var contenedor = $('#div-contact'); //div en html donde se guardara todos los contactos
 
+    //limpio los campos del formulario
+    $('.name').val('');
+    $('.phone').val('');
+    $('.mail').val('');
 
-  });
+    //Creo el div que contendrá los datos del contacto: name, phone y email
+    contenedor.append('<div class="container contactc"><ul class="collection"><li class="collection-item avatar"><h5>' + name +'</h5><p>'+ phone +'</p><h5>' + email +'</h5><a href="#!" class="secondary-content"><i class="material-icons trash">delete_forever</i></a></li></ul></div>');
+    /* estructura del div
+    <div class="container contact">
+      <ul class="collection">
+        <li class="collection-item avatar">
+          <h5>name</h5>
+          <p>phone</p>
+          <h5>email</h5>
+          <a href="#!" class="secondary-content"><i class="material-icons trash">delete_forever</i></a>
+        </li>
+      </ul>
+      </div> */  
+      deleteContact(); //se llama a la funcion borrar
+
+    });
+
+    function deleteContact(){//funcion que borra un contacto
+
+    $('.trash').click(function(){//evento click en botón borrar de un contacto
+
+      $(this).parent().parent().parent().remove();//this apunta al boton eliminar
+      // y parent() permite eliminar los contenedores de ese botón
+    })
+  }
+});
